ebf7765bd9408f44496b8725588674f490dc7e13
Matthew Reed <mattreed1890@gmail.com>
2017-11-28 17:41:39 -0600

    Initial Commit

---
+++ HS Visit example project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HS Visit example project
+//
+//  Created by Matthew Reed on 11/28/17.
+//  Copyright © 2017 Matthew Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
